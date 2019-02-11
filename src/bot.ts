c7c48276b381c74a6c2c98e99e5b07626f677d5f
Artur <exstrominer@gmail.com>
2019-02-12 00:06:24 +0300

    Add errors handle

--- src/bot.ts
+++ src/bot.ts
@@ -86,192 +86,246 @@ const hearCommand = (
 };
 
 hearCommand('alive', ['/alive'], (context: any) => {
-  context.sendSticker(9046);
+  try {
+    context.sendSticker(9046);
+  } catch (err) {
+    context.send(
+      '❌ An unknown error occurred while trying to execute a command',
+    );
+  }
 });
 
 hearCommand('help', ['/help'], (context: any) => {
-  context.send(`
-    📋 List of available commands:
-    ➡ /help - Show available commands
-    ➡ /alive - Check bot status
-    ➡ /tt - Give available time table
-    ➡ /cs1 - Give call schedule for first block
-    ➡ /cs2 - Give call schedule for second block
-    ➡ /fact - Give random fact from list
-    ➡ /hook - Hook random chat user (Admin permissions only)
-    ➡ /vika - Command for fun
-  `);
+  try {
+    context.send(`
+      📋 List of available commands:
+      ➡ /help - Show available commands
+      ➡ /alive - Check bot status
+      ➡ /tt - Give available time table
+      ➡ /cs1 - Give call schedule for first block
+      ➡ /cs2 - Give call schedule for second block
+      ➡ /fact - Give random fact from list
+      ➡ /hook - Hook random chat user (Admin permissions only)
+      ➡ /vika - Command for fun
+    `);
+  } catch (err) {
+    context.send(
+      '❌ An unknown error occurred while trying to execute a command',
+    );
+  }
 });
 
 hearCommand(
   'timetable',
   ['/tt', '/timetable', '/raspisanie', '/rasp', '/расписание', '/расп'],
   async (context: any) => {
-    // Get image url
-    const $ = await rp.get(`${ENDPOINT}/raspisanie`, {
-      transform: (body: string) => cheerio.load(body),
-    });
+    try {
+      // Get image url
+      const $ = await rp.get(`${ENDPOINT}/raspisanie`, {
+        transform: (body: string) => cheerio.load(body),
+      });
 
-    const imageUrl = $('.page_raspis_block_img')
-      .find('img')
-      .first()
-      .attr('src');
+      const imageUrl = $('.page_raspis_block_img')
+        .find('img')
+        .first()
+        .attr('src');
 
-    // Get image
-    const imgBuffer = await getRawImage(imageUrl);
+      // Get image
+      const imgBuffer = await getRawImage(imageUrl);
 
-    // Load to sharp
-    const sharpImg = sharp(imgBuffer);
+      // Load to sharp
+      const sharpImg = sharp(imgBuffer);
 
-    // Check image size
-    const imgInfo = await sharpImg.metadata();
+      // Check image size
+      const imgInfo = await sharpImg.metadata();
 
-    // Modify image
-    const modifiedImgBuffer = await sharpImg
-      .extract({
-        left: 0,
-        top: imgInfo.height - 177, // calculated image size
-        width: 129,
-        height: 177,
-      }) // Extract table
-      .resize(258, 354) // Resize x2
-      .toBuffer();
+      // Modify image
+      const modifiedImgBuffer = await sharpImg
+        .extract({
+          left: 0,
+          top: imgInfo.height - 177, // calculated image size
+          width: 129,
+          height: 177,
+        }) // Extract table
+        .resize(258, 354) // Resize x2
+        .toBuffer();
 
-    const attachmentPhoto = await vk.upload.messagePhoto({
-      source: modifiedImgBuffer,
-    });
+      const attachmentPhoto = await vk.upload.messagePhoto({
+        source: modifiedImgBuffer,
+      });
 
-    context.send({
-      attachment: attachmentPhoto,
-    });
+      context.send({
+        attachment: attachmentPhoto,
+      });
+    } catch (err) {
+      context.send(
+        '❌ An unknown error occurred while trying to execute a command',
+      );
+    }
   },
 );
 
 hearCommand('callSchedule1', ['/cs1', '/call1'], async (context: any) => {
-  // Get image
-  const [imgBufferTuesday, imgBufferOther] = await Promise.all([
-    getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_1_2.png`),
-    getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_1_1.png`),
-  ]);
+  try {
+    // Get image
+    const [imgBufferTuesday, imgBufferOther] = await Promise.all([
+      getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_1_2.png`),
+      getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_1_1.png`),
+    ]);
 
-  // Combine images
-  const combinedImg = await merge([imgBufferOther, imgBufferTuesday]);
+    // Combine images
+    const combinedImg = await merge([imgBufferOther, imgBufferTuesday]);
 
-  // Attach photo
-  const attachmentPhoto = await vk.upload.messagePhoto({
-    source: combinedImg,
-  });
+    // Attach photo
+    const attachmentPhoto = await vk.upload.messagePhoto({
+      source: combinedImg,
+    });
 
-  // Send message
-  context.send({
-    attachment: attachmentPhoto,
-  });
+    // Send message
+    context.send({
+      attachment: attachmentPhoto,
+    });
+  } catch (err) {
+    context.send(
+      '❌ An unknown error occurred while trying to execute a command',
+    );
+  }
 });
 
 hearCommand('callSchedule1', ['/cs2', '/call2'], async (context: any) => {
-  // Get image
-  const [imgBufferTuesday, imgBufferOther] = await Promise.all([
-    getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_2_2.png`),
-    getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_2_1.png`),
-  ]);
+  try {
+    // Get image
+    const [imgBufferTuesday, imgBufferOther] = await Promise.all([
+      getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_2_2.png`),
+      getRawImage(`${ENDPOINT}/wp-content/themes/politeh/image/Korpus_2_1.png`),
+    ]);
 
-  // Combine images
-  const combinedImg = await merge([imgBufferOther, imgBufferTuesday]);
+    // Combine images
+    const combinedImg = await merge([imgBufferOther, imgBufferTuesday]);
 
-  // Attach photo
-  const attachmentPhoto = await vk.upload.messagePhoto({
-    source: combinedImg,
-  });
+    // Attach photo
+    const attachmentPhoto = await vk.upload.messagePhoto({
+      source: combinedImg,
+    });
 
-  // Send message
-  context.send({
-    attachment: attachmentPhoto,
-  });
+    // Send message
+    context.send({
+      attachment: attachmentPhoto,
+    });
+  } catch (err) {
+    context.send(
+      '❌ An unknown error occurred while trying to execute a command',
+    );
+  }
 });
 
 hearCommand('hook', ['/hook', '/h'], async (context: MessageContext) => {
-  const members = await vk.api.messages.getConversationMembers({
-    peer_id: context.peerId,
-  });
+  try {
+    const members = await vk.api.messages.getConversationMembers({
+      peer_id: context.peerId,
+    });
 
-  // Get random profile
-  const randomProfile =
-    members.profiles[getRandomInt(0, members.profiles.length)];
+    // Get random profile
+    const randomProfile =
+      members.profiles[getRandomInt(0, members.profiles.length)];
 
-  context.send(
-    `Get over here - ${randomProfile.first_name} ${randomProfile.last_name}`,
-  );
+    context.send(
+      `🎣 Get over here - ${randomProfile.first_name} ${
+        randomProfile.last_name
+      }`,
+    );
+  } catch (err) {
+    if (err.code === 917) {
+      context.send('❌ To use this command, bot requires admin rights');
+    } else {
+      context.send(
+        '❌ An unknown error occurred while trying to execute a command',
+      );
+    }
+  }
 });
 
 let vikaTimeout;
 hearCommand('vika', ['/vika', '/roflan'], async (context: MessageContext) => {
-  const emotesIds = [
-    1001738,
-    150380,
-    958403,
-    90896,
-    117708,
-    1678208,
-    41135,
-    118081,
-    975930,
-    1844320,
-    1722639,
-    59918,
-    1388990,
-    1381423,
-    771070,
-    1201063,
-    669957,
-    181421,
-    43996,
-    117092,
-    117709,
-    332579,
-    1191320,
-    1228008,
-  ];
+  try {
+    const emotesIds = [
+      1001738,
+      150380,
+      958403,
+      90896,
+      117708,
+      1678208,
+      41135,
+      118081,
+      975930,
+      1844320,
+      1722639,
+      59918,
+      1388990,
+      1381423,
+      771070,
+      1201063,
+      669957,
+      181421,
+      43996,
+      117092,
+      117709,
+      332579,
+      1191320,
+      1228008,
+    ];
 
-  if (Date.now() < vikaTimeout) {
-    context.send(
-      `Arthas takes out the fucking trash (${ms(vikaTimeout - Date.now())})`,
+    if (Date.now() < vikaTimeout) {
+      context.send(
+        `Arthas takes out the fucking trash (${ms(vikaTimeout - Date.now())})`,
+      );
+      return;
+    }
+    vikaTimeout = Date.now() + ms('2h');
+
+    // Get image
+    const imgBuffer = await getRawImage(
+      `https://static-cdn.jtvnw.net/emoticons/v1/${
+        emotesIds[getRandomInt(0, emotesIds.length)]
+      }/3.0`,
     );
-    return;
+
+    // Attach photo
+    const attachmentPhoto = await vk.upload.messagePhoto({
+      source: imgBuffer,
+    });
+
+    // Attach audio
+    const attachmentAudio = new AudioAttachment(
+      {
+        id: 456239689,
+        owner_id: 2000421094,
+      },
+      vk,
+    );
+
+    // Send message
+    context.send({
+      attachment: [attachmentAudio.toString(), attachmentPhoto.toString()],
+    });
+  } catch (err) {
+    context.send(
+      '❌ An unknown error occurred while trying to execute a command',
+    );
   }
-  vikaTimeout = Date.now() + ms('2h');
-
-  // Get image
-  const imgBuffer = await getRawImage(
-    `https://static-cdn.jtvnw.net/emoticons/v1/${
-      emotesIds[getRandomInt(0, emotesIds.length)]
-    }/3.0`,
-  );
-
-  // Attach photo
-  const attachmentPhoto = await vk.upload.messagePhoto({
-    source: imgBuffer,
-  });
-
-  // Attach audio
-  const attachmentAudio = new AudioAttachment(
-    {
-      id: 456239689,
-      owner_id: 2000421094,
-    },
-    vk,
-  );
-
-  // Send message
-  context.send({
-    attachment: [attachmentAudio.toString(), attachmentPhoto.toString()],
-  });
 });
 
 hearCommand('fact', ['/fact', '/f'], async (context: MessageContext) => {
-  const randomFact = facts[getRandomInt(0, facts.length)];
+  try {
+    const randomFact = facts[getRandomInt(0, facts.length)];
 
-  // Send message
-  context.send(randomFact);
+    // Send message
+    context.send(randomFact);
+  } catch (err) {
+    context.send(
+      '❌ An unknown error occurred while trying to execute a command',
+    );
+  }
 });
 
 function getRawImage(url) {
