799774dcaf26bfa572baa7c62828caff51b078ae
Artur <exstrominer@gmail.com>
2019-03-13 18:37:00 +0300

    refactor(db): use promise instead callback

--- src/db.ts
+++ src/db.ts
@@ -3,11 +3,10 @@ import { connect } from 'mongoose';
 connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true },
-  err => {
-    if (err) {
-      throw err;
-    }
-
-    console.log('Success connected to db');
-  },
-);
+)
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch(err => {
+    throw err;
+  });
