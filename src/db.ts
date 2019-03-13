45270e5105e10cc7edcf2cfcda93c6a0206877e4
Artur <exstrominer@gmail.com>
2019-03-13 19:19:36 +0300

    chore(db): disable mongoose warning

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,10 @@ import { connect } from 'mongoose';
 
 connect(
   process.env.MONGO_URL,
-  { useNewUrlParser: true },
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  },
 )
   .then(() => {
     console.log('Database connection successful');
