5168c174242990d64923325cb230b9ca499db642
Artur <exstrominer@gmail.com>
2019-03-18 20:16:29 +0300

    chore(config): change config settings

--- config/default.js
+++ config/default.js
@@ -3,14 +3,13 @@ module.exports = {
     maxDuplicatesWarning: 5,
     maxDuplicatesPenalize: 7,
     penalizeExpCount: 20,
-    warnBuffer: 4,
-    penalizeBuffer: 6,
-    interval: 6000,
+    warnBuffer: 5,
+    penalizeBuffer: 7,
+    interval: 5000,
   },
   messageTypeXP: {
     photo: 1,
     audio: 1,
     video: 1,
-    gift: 10,
   },
 };
