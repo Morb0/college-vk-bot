12d630d55de8ffc4cbb21e9a7a3356568bb13f0d
Artur <exstrominer@gmail.com>
2019-03-18 12:05:27 +0300

    fix(maintenance): use blank string when no dev ids

--- src/middlewares/maintenance.ts
+++ src/middlewares/maintenance.ts
@@ -9,7 +9,7 @@ export const maintenanceCheck = async (
   const isMaintenance =
     !!process.env.MAINTENANCE && process.env.MAINTENANCE !== '0';
   if (isMaintenance) {
-    const devPeerIds = process.env.DEV_PEER_IDS.split(',');
+    const devPeerIds = (process.env.DEV_PEER_IDS || '').split(',');
     if (devPeerIds.indexOf(context.peerId.toString()) === -1) {
       if (process.env.MAINTENANCE === '1') {
         await context.send(`🚧 ${t('MAINTENANCE')}`);
