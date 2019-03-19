ad2317eccd55e7cdddf19bd8f777ebd67295b84b
Artur <exstrominer@gmail.com>
2019-03-19 10:19:58 +0300

    fix(translate): set correct lang dir path

--- packages/bot/src/utils/translate.ts
+++ packages/bot/src/utils/translate.ts
@@ -1,6 +1,6 @@
 const translations: { [key: string]: { [key: string]: string } } = {
-  en: require('../lang/en.json'),
-  ru: require('../lang/ru.json'),
+  en: require('../../lang/en.json'),
+  ru: require('../../lang/ru.json'),
 };
 
 export const t = (tag: string): string => {
