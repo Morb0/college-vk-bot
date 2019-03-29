53d04c678aa0d2e0f9627136a2d7f9bf49445527
Artur <exstrominer@gmail.com>
2019-03-30 01:16:26 +0300

    fix(ranks-top): add space

--- src/commands/ranks-top.ts
+++ src/commands/ranks-top.ts
@@ -24,10 +24,11 @@ const handler = async (context: MessageContext) => {
     const foundUser = await User.findOne({ vkId: foundChatsXP[key].vkId });
     topList += `${emojiNums[key]} ${foundUser.firstName} ${
       foundUser.lastName
-    }${!!foundChatsXP[key].stars ?
-      new Array(foundChatsXP[key].stars + 1).join('⭐') + ' ' : ''}- ${
-      foundChatsXP[key].xp
-    } ${t('XP')}\n`;
+    } ${
+      !!foundChatsXP[key].stars
+        ? new Array(foundChatsXP[key].stars + 1).join('⭐') + ' '
+        : ''
+    }- ${foundChatsXP[key].xp} ${t('XP')}\n`;
   }
 
   await context.send(`
